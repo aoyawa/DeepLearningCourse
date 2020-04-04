035db8643882ee83caf1ffd4137846c7842cd078
ole-byte <2521451059@qq.com>
2020-04-05 01:06:41 +0800

    第三周作业test55_2.py被误删，重新上传

--- test.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import tensorflow as tf
-mnist=tf.keras.datasets.mnist
-(train_x,train_y),(test_x,test_y)=mnist.load_data()
-print("train_set",len(train_x))
-print('test_set',len(test_x))
